73acc71e48953093ab1ae231654a2d5eddbf757a
Le Phu Cuong <cuongle6105@gmail.com>
2026-03-24 10:20:22 +0700

    create project

---
+++ src/main/java/com/lpcuong/jobhub_web/JobhubWebApplication.java
@@ -0,0 +1,13 @@
+package com.lpcuong.jobhub_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobhubWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhubWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lpcuong/jobhub_web/JobhubWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpcuong.jobhub_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhubWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
